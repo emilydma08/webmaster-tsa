686dba89a0b2a9df26984ed844fe859e5e481a33
emilydma08 <142754713+emilydma08@users.noreply.github.com>
2024-11-16 11:58:09 -0800

    Created basic FAQ layout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Pop-Up JS
 document.addEventListener('DOMContentLoaded', function () {
     const popupOverlay = document.getElementById('popupOverlay');
     const popup = document.getElementById('popup');
@@ -22,6 +23,26 @@ document.addEventListener('DOMContentLoaded', function () {
     popupOverlay.addEventListener('click', function (event) {
         if (event.target === popupOverlay) {
             closePopupFunc();
+        }
+    });
+});
+
+//FAQ JS
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+faqQuestions.forEach((question) => {
+    question.addEventListener('click', function() {
+        const answer = this.nextElementSibling;
+        const toggleSign = this.querySelector('.toggle-sign');
+
+        if (answer.style.display === "block" || answer.classList.contains('open')) {
+            answer.style.display = "none";
+            answer.classList.remove('open');
+            toggleSign.textContent = "+";
+        } else {
+            answer.style.display = "block";
+            answer.classList.add('open');
+            toggleSign.textContent = "−";
         }
     });
 });
